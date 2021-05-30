52dff16b4834bfc25fbedb7814bd8bc01ebabe81
rolandmontalvan <rolandfilho@gmail.com>
2021-05-30 16:15:05 -0300

    ultima atualizacao

--- schema.js
+++ schema.js
@@ -1,83 +1,114 @@
-const {
-    GraphQLObjectType,
-    GraphQLInt,
-    GraphQLBoolean,
-    GraphQLString,
-    GraphQLList,
-    GraphQLSchema,
-  } = require("graphql");
-  
-  const axios = require("axios");
-  
+const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLBoolean, GraphQLList, GraphQLSchema } = require('graphql');
+const axios = require('axios');
   // Launch Type
-  const LaunchType = new GraphQLObjectType({
-    name: "Launch",
+const LaunchType = new GraphQLObjectType({
+    name: 'Launch',
     fields: () => ({
-      flight_number: { type: GraphQLInt },
-      mission_name: { type: GraphQLString },
-      launch_year: { type: GraphQLString },
-      launch_date_local: { type: GraphQLString },
-      launch_success: { type: GraphQLBoolean },
-      rocket: { type: RocketType },
-    }),
-  });
-  
-  // Rocket Type
-  const RocketType = new GraphQLObjectType({
-    name: "Rocket",
+        flight_number: { type: GraphQLInt },
+        mission_name: { type: GraphQLString },
+        launch_year: { type: GraphQLString },
+        launch_date_local: { type: GraphQLString },
+        launch_success: { type: GraphQLBoolean },
+        rocket: { type: RocketType }
+    })
+});
+
+// Rocket Type
+const RocketType = new GraphQLObjectType({
+    name: 'Rocket',
     fields: () => ({
-      rocket_id: { type: GraphQLString },
-      rocket_name: { type: GraphQLString },
-      rocket_type: { type: GraphQLString },
-    }),
-  });
-  
-  // Root Query
-  const RootQuery = new GraphQLObjectType({
-    name: "RootQueryType",
+        rocket_id: { type: GraphQLString },
+        rocket_name: { type: GraphQLString },
+        rocket_type: { type: GraphQLString },
+        wikipedia: { type: GraphQLString },
+        description: { type: GraphQLString }
+    })
+});
+
+// History Type
+const HistoryType = new GraphQLObjectType({
+    name: 'History',
+    fields: () => ({
+        id: { type: GraphQLInt },
+        title: { type: GraphQLString },
+        event_date_utc: { type: GraphQLString },
+        details: { type: GraphQLString },
+        links: { type: LinksType }
+    })
+});
+
+// Links Type
+const LinksType = new GraphQLObjectType({
+    name: 'Links',
+    fields: () => ({
+        reddit: { type: GraphQLString },
+        article: { type: GraphQLString },
+        wikipedia: { type: GraphQLString }
+    })
+});
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
     fields: {
-      launches: {
-        type: new GraphQLList(LaunchType),
-        resolve(parent, args) {
-          return axios
-            .get("https://api.spacexdata.com/v4/launches")
-            .then((res) => res.data);
+        launches: {
+            type: new GraphQLList(LaunchType),
+            resolve(parent, args) {
+                return axios
+                    .get('https://api.spacexdata.com/v3/launches')
+                    .then(res => res.data);
+            }
         },
-      },
-      launch: {
-        type: LaunchType,
-        args: {
-          flight_number: { type: GraphQLInt },
+        launch: {
+            type: LaunchType,
+            args: {
+                flight_number: { type: GraphQLInt }
+            },
+            resolve(parent, args) {
+                return axios
+                    .get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
+                    .then(res => res.data);
+            }
         },
-        resolve(parent, args) {
-          return axios
-            .get(`https://api.spacexdata.com/v4/launches/${args.flight_number}`)
-            .then((res) => res.data);
+        rockets: {
+            type: new GraphQLList(RocketType),
+            resolve(parent, args) {
+                return axios
+                    .get('https://api.spacexdata.com/v3/rockets')
+                    .then(res => res.data);
+            }
         },
-      },
-      rockets: {
-        type: new GraphQLList(RocketType),
-        resolve(parent, args) {
-          return axios
-            .get("https://api.spacexdata.com/v4/rockets")
-            .then((res) => res.data);
+        rocket: {
+            type: RocketType,
+            args: {
+                rocket_id: { type: GraphQLString }
+            },
+            resolve(parent, args) {
+                return axios
+                    .get(`https://api.spacexdata.com/v3/rockets/${args.rocket_id}`)
+                    .then(res => res.data);
+                }
         },
-      },
-      rocket: {
-        type: RocketType,
-        args: {
-          id: { type: GraphQLInt },
+        histories: {
+            type: new GraphQLList(HistoryType),
+            resolve(parent, args) {
+                return axios
+                    .get('https://api.spacexdata.com/v3/history')
+                    .then(res => res.data);
+            }
         },
-        resolve(parent, args) {
-          return axios
-            .get(`https://api.spacexdata.com/v4/rockets/${args.id}`)
-            .then((res) => res.data);
-        },
-      },
-    },
-  });
-  
-  module.exports = new GraphQLSchema({
-    query: RootQuery,
-  });
-  
+        history: {
+            type: HistoryType,
+            args: {
+                id: { type: GraphQLInt }
+            },
+            resolve(parent, args) {
+                return axios
+                    .get(`https://api.spacexdata.com/v3/history/${args.id}`)
+                    .then(res => res.data);
+                }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({ query: RootQuery });
--- server.js
+++ server.js
@@ -1,27 +1,26 @@
-const express = require("express");
-const graphQLHttp = require("express-graphql");
-const schema = require("./schema");
-const cors = require("cors");
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const cors = require('cors');
+const schema = require('./schema');
+const path = require('path');
 const app = express();
-const path = require("path");
 
 app.use(cors());
 
 app.use(
-  "/graphql",
-  graphQLHttp({
-    schema,
-    graphiql: true,
-  })
+    '/graphql',
+    graphqlHTTP({
+        schema,
+        graphiql: true
+    })
 );
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
-});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
